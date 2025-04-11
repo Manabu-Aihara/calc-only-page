438f31056c9058a3ca5c1dd64fbf12bf9fff5dc8
Manabu-Aihara <manabu@yoboiryo.co.jp>
2025-04-11 14:25:39 +0900

    feat #250411: とりあえず完成！

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from pydantic import BaseModel
 from pathlib import Path
+import uuid
+from datetime import datetime
 
 from sqlalchemy.orm import Session
 from fastapi import Depends, FastAPI, HTTPException, status, Request, Form
@@ -45,7 +47,7 @@ class Token(BaseModel):
     token_type: str
 
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token_id")
 
 
 def get_user(user_id: int):
@@ -83,7 +85,7 @@ async def decord_token_data(token: str = Depends(oauth2_scheme)):
         print(f"Pass 2: {e}")
         raise credentials_exception
     except Exception as e:
-        print(e)
+        print(f"func: {e}")
     if user_id is None:
         print("Pass 3")
         raise credentials_exception
@@ -101,6 +103,8 @@ async def read_users_me(request: Request):
     token_store[param_token] = param_token
 
     # リダイレクト時にトークンIDのみを渡す
+    # 303 See Other - Must
+    # https://stackoverflow.com/questions/73076517/how-to-send-redirectresponse-from-a-post-to-a-get-route-in-fastapi
     return RedirectResponse(
         url=f"/receive-data?token_id={param_token}",
         status_code=status.HTTP_303_SEE_OTHER,
@@ -116,37 +120,48 @@ templates = Jinja2Templates(directory=str(Path(BASE_DIR, "templates")))
 
 @app.get("/receive-data")
 async def receive_data(request: Request):
+    # リダイレクトがダメなのかなぁ？
+    # async def receive_data(request: Request, 🙅‍♀user_id: int = Depends(decord_token_data)):
     token_id = request.query_params.get("token_id")
-    if not token_id:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, detail="Token ID is missing"
-        )
 
     # 保存されたトークンを取得
-    token = token_store.get(token_id)
-    if not token:
+    stored_token = token_store.get(token_id)
+    if not stored_token:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token ID"
         )
-
     # トークンを検証
     try:
-        user_id = await decord_token_data(token)
-        # 使用済みトークンを削除
-        del token_store[token_id]
+        user_id = await decord_token_data(stored_token)
+        print(f"ID: {user_id}")
+        # return {"user id": user_id}
         user_info = (
             session.query(models.User.LKANA)
             .filter(models.User.STAFFID == user_id)
             .first()
         )
         team_code_list = session.query(models.Team.CODE).all()
-        # return {"user id": user_id}
+
+        # 新しいトークンIDを生成
+        new_token_id = str(uuid.uuid4())
+        # ?
+        token_store[new_token_id] = {"token": stored_token}
+        print(f"Token new or old: {token_store[new_token_id]}")
+
+        # 使用済みトークンを削除
+        del token_store[stored_token]
+
         return templates.TemplateResponse(
             "select_form.html",
-            {"name": user_info.LKANA, "team_nums": team_code_list, "request": request},
+            {
+                "request": request,
+                "name": user_info.LKANA,
+                "team_nums": team_code_list,
+                "new_token": new_token_id,
+            },
         )
     except Exception as e:
-        print(e)
+        print(f"app: {e}")
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token"
         )
@@ -154,6 +169,69 @@ async def receive_data(request: Request):
 
 @app.post("/collect-members-data")
 async def post_collected_dict(request: Request, team_number: str = Form(...)):
-    # return {"team code": team_number}
     result_df = put_vertical_dataframe(team_code=int(team_number))
-    return result_df.to_html()
+    return result_df.T.to_dict()
+
+
+@app.post("/collect-members-html")
+async def output_collected_html(
+    request: Request, team_number: str = Form(...), form_token: str = Form(...)
+):
+    # team_number = form_data.team_number
+    print(f"Form token: {form_token}")
+    result_df = put_vertical_dataframe(team_code=int(team_number))
+    setup_html = """
+    <!DOCTYPE html>
+    <html>
+
+    <head>
+        <link rel="stylesheet" href="https://cdn.datatables.net/2.2.2/css/dataTables.dataTables.css" />
+
+        <script src="https://code.jquery.com/jquery-3.7.1.min.js"
+            integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+        <script src="https://cdn.datatables.net/2.2.2/js/dataTables.js"></script>
+        <script>
+            $(document).ready(function () {
+                $('.dataframe').DataTable();
+            });
+        </script>
+    </head>
+
+    <body>
+        <div>
+    """
+    table_tag_html = result_df.T.to_html()
+    teardown_html = """
+        </div>
+    </body>
+
+    </html>
+    """
+    output_html = setup_html + table_tag_html + teardown_html
+    dateime_format = datetime.today().strftime("%Y%m%d%H%M")
+    file_name = f"{team_number}-{dateime_format}"
+
+    # https://note.nkmk.me/python-pathlib-file-open-read-write-unlink/
+    # 新規ファイルを作成する場合は直上のディレクトリまでは作成しておく必要がある。
+    out_path = Path(f"app/templates/output_{file_name}.html")
+    with out_path.open(mode="w") as f:
+        f.write(output_html)
+
+    return RedirectResponse(
+        f"/output-table?file_name={file_name}&token_id={form_token}",
+        status_code=status.HTTP_303_SEE_OTHER,
+    )
+
+
+@app.get("/output-table")
+def display_table(request: Request):
+    token_id = request.query_params.get("token_id")
+    if not token_id:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Token ID is missing"
+        )
+    group_and_date = request.query_params.get("file_name")
+    del token_store[token_id]
+    return templates.TemplateResponse(
+        f"output_{group_and_date}.html", {"request": request}
+    )
